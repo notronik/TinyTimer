eee51a469aa68097128b3798ca914da52af0830e
Nik Vangerow <n@notro.uk>
2016-05-10 13:35:52 +0100

    Initial Commit

---
+++ Timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Timer
+//
+//  Created by Nik on 10/05/2016.
+//  Copyright © 2016 notro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Nik on 10/05/2016.
+//  Copyright © 2016 notro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
